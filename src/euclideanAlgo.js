ade4cea8b5b6a6c17cbeec9d8a3c0bb602056627
ChristianBraunagel <62561756+ChristianBraunagel@users.noreply.github.com>
2022-06-11 19:54:29 +0200

    checks for input parameters

--- src/euclideanAlgo.js
+++ src/euclideanAlgo.js
@@ -1,4 +1,16 @@
-export function euclideanAlgo(){
+export function euclideanAlgo(a, b){
+
+    // at least one parameter is missing
+    if( (a === undefined) || (b === undefined) ){
+        return 'The euclidean algo needs two input parameters.'
+    }
+
+    // a or b is not a natural number > 0
+    if( (a < 1) || (b < 1) || (!Number.isInteger(a)) || (!Number.isInteger(b)) ){
+        return 'The euclidean algo is only defined for natural numbers > 0.';
+    }
+
+    
 
     return 0;
 }
